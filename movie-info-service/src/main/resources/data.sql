87abf35280990af5215e7179d98f45a56387f016
sratoori <suresh.ratoori@gmail.com>
2019-09-27 00:28:54 -0400

    Adding Eureka Client along with registered in Service Discovery

--- movie-info-service/src/main/resources/data.sql
+++ movie-info-service/src/main/resources/data.sql
@@ -1,2 +1,4 @@
+insert into Movie values ('100', 'TimeMachine', 'TimeMachine Movie')
+insert into Movie values ('200', 'Chichore', 'Chichore Movie')
 insert into Movie values ('300', 'Transformer', 'Transformer Movie')
 insert into Movie values ('400', 'DreamGirl', 'DreamGirl Movie')
